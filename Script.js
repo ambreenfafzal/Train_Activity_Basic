1435ff23acf26c4f15b9cc5c3a0b272f243622f1
aafzal <ambreenfafzal@gmail.com>
2019-04-28 01:06:39 -0500

    s is homework for the train activity

---
+++ Script.js
@@ -0,0 +1,12 @@
+
+//This function grabs the value of the input fields
+$(document).on("click", function(){
+ var trainName = $("#train-name").val();
+ var destinationName = $("#destination-name").val();
+ var trainTime = $("#time-entered").val();
+ console.log(trainName);
+ console.log(destinationName);
+ console.log(trainTime);
+
+ var trainFreq = 
+});
